e8019879e03b2385a5476b26a71242271899794a
alantga <alan.ellis@telegraphacademy.com>
2016-05-10 17:21:11 -0700

    Create circle.js

---
+++ circle.js
@@ -0,0 +1,7 @@
+console.log('Alan\'s NPM package was loaded...');
+
+module.exports = {
+  positivelyreinforce : function() {
+   console.log('The world needs more people like you. Your spirit and strength are infectious to others.');
+ }
+};

0ca91e7b5a9a7e21368020ebb77484c3b06b93e9
alantga <alan.ellis@telegraphacademy.com>
2016-05-10 17:23:47 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const circle = require('./circle');
+
+console.log('bring about positive reinforcement, please');
+console.log('positive reinforcement()');
+circle.positivereinforcement();
+
+module.exports = {
+  positivereinforcement: circle.positivereinforcement
+}
